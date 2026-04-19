3937c625ec368c9077fe56212695b33cb005770c
Paulo Vargas <paulotomegomesdevargas@gmail.com>
2026-04-19 15:01:36 -0300

    Implementado extrato e garantindo persistencia dos dados

--- src/main/java/br/com/bank/dao/ExtratoDAO.java
+++ src/main/java/br/com/bank/dao/ExtratoDAO.java
@@ -1,4 +1,22 @@
 package br.com.bank.dao;
 
+import javax.ejb.Stateless;
+import javax.persistence.PersistenceContext;
+
+@Stateless
 public class ExtratoDAO {
+
+    @PersistenceContext(unitName = "BankPU")
+    private javax.persistence.EntityManager em;
+
+    public void salvar(br.com.bank.entity.Extrato extrato) {
+        em.persist(extrato);
+    }
+
+    public java.util.List<br.com.bank.entity.Extrato> buscarPorConta(Long contaId) {
+        return em.createQuery("SELECT e FROM Extrato e WHERE e.conta.id = :contaId ORDER BY e.dataHora DESC", br.com.bank.entity.Extrato.class)
+                .setParameter("contaId", contaId)
+                .getResultList();
+    }
+
 }
--- src/main/java/br/com/bank/entity/Extrato.java
+++ src/main/java/br/com/bank/entity/Extrato.java
@@ -1,4 +1,39 @@
 package br.com.bank.entity;
 
-public class Extrato {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "extrato")
+public class Extrato implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "extrato_seq")
+    @SequenceGenerator(name = "extrato_seq", sequenceName = "extrato_seq", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_id", nullable = false)
+    private Conta conta;
+
+    @Column(nullable = false)
+    private java.time.LocalDateTime dataHora;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private java.math.BigDecimal valor;
+
+    @Column(nullable = false)
+    private String operacao; // Ex: DEBITO, CREDITO
 }
